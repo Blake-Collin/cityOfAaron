e369de5194e5554c901ab818319cb4a7b2604765
Collin Blake <collinblake1@gmail.com>
2018-10-09 16:18:44 -0600

    10-09-2018 Created Program

---
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,19 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Fall 2018
+ * Team members: Collin Blake, Jacob Gallegos, Daniel Martin
+ */
+package cityofaaron;
+
+
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
