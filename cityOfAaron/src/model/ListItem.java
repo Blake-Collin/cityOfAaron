1ca5555584c1d1e9f483d2c57412e35f8121c4d9
starr06 <35233209+starr06@users.noreply.github.com>
2018-10-11 11:39:04 -0500

    ListItem updated

--- cityOfAaron/src/model/ListItem.java
+++ cityOfAaron/src/model/ListItem.java
@@ -1,11 +1,35 @@
 package model;
 
+import java.io.Serializable;
+
 /*
  * The main() class file for the cityOfAaron project
  * CIT-260
  * Fall 2018
  * Team members: Collin Blake, Jacob Gallegos, Daniel Martin
  */
-public class ListItem {
 
+public class ListItem implements Serializable{
+    
+    //Variables
+    private String name;
+    private Integer number;
+    
+    //Functions
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+    
 }
