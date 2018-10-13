2d58b2592f47a2bbb665c69680ef3c484d138eba
Jacob Gallegos <gal16020@byui.edu>
2018-10-13 13:20:55 -0600

    Individual Assignment I updated the Location class and in the Main class I implemented the class to output. Nothing else was changed besides the Location Class and the Main.

--- cityOfAaron/src/model/Location.java
+++ cityOfAaron/src/model/Location.java
@@ -1,10 +1,39 @@
 package model;
+import java.io.Serializable;
 
 /*
  * CIT-260
  * Fall 2018
  * Team members: Collin Blake, Jacob Gallegos, Daniel Martin
  */
-public class Location {
+public class Location implements Serializable {
+    
+    //Variables
+    private String description;
+    private String symbol;
+    
+    //constructor
+    public Location(){}
+    
+    //Methods
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+    
+    
+    
 
 }
