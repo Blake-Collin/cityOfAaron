8a960a84b94011f24e8b657688c00e61c6f03a2a
starr06 <35233209+starr06@users.noreply.github.com>
2018-11-15 16:14:11 -0600

    Merge origin/master
    Conflicts:
    	cityOfAaron/src/model/ListItem.java

--- cityOfAaron/src/view/CropView.java
+++ cityOfAaron/src/view/CropView.java
@@ -129,6 +129,27 @@ public class CropView {
         
     }
     
+        /**
+    * The plantCropsView method
+    * Purpose: interface with the user input for planting crops
+    * Parameters: none
+    * Returns: none
+    */
+    public static void plantCropsView()
+    {
+        // Ask the user “How many acres of land do you want to plant?”
+        System.out.print("How many acres of land do you wish to plant?");
+        
+        // Get the user’s input and save it.
+        int toPlant;
+        toPlant = keyboard.nextInt();
+        
+        // Call the plantCrops( ) method in the control layer to plant crops
+        CropControl.plantCrops(toPlant, cropData);
+                
+    }
+    
+    
     
 }
 
